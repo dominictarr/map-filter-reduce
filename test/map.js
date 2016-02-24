09723f9459ec850ad9de5c9f607959c65216fb40
Dominic Tarr <dominic.tarr@gmail.com>
2016-02-24 15:27:37 +1300

    additional test cases for blobs

--- test/map.js
+++ test/map.js
@@ -1,9 +1,13 @@
 
 var tape = require('tape')
-var map = require('../map')
+var make = require('../map')
 var inputs = [
   'string'
 ]
+
+function map(q, v) {
+  return make(q)(v)
+}
 
 tape('map', function (t) {
   t.deepEqual(map(true, true), true)
@@ -13,9 +17,21 @@ tape('map', function (t) {
   t.deepEqual(map({bar: 'bar'}, {foo: 1, bar: 2}), {bar: 2})
   t.deepEqual(map({f: 'bar', b: 'foo'}, {foo: 1, bar: 2}), {f: 2, b: 1})
   t.deepEqual(map(
-    {abc: ['a', 'b', 'c', {foo: true, bar: true}]},
+    {foo: true, bar: true},
+    {foo:1, bar:2, baz: 3}
+  ), {foo:1, bar: 2})
+  t.deepEqual(map(
+    {abc: ['a', 'b', 'c', true]}, //{foo: true, bar: true}
     {a: {b: {c: {foo:1, bar:2, baz: 3}}}}
-  ), {abc: {foo:1, bar: 2}})
+  ), {abc: {foo:1, bar: 2, baz: 3}})
+  t.end()
+})
+
+tape('array is repeated map', function (t) {
+  t.deepEqual(map(
+    ['a', 'b', 'c'],
+    {a: {b: {c: {foo:1, bar:2}}}}
+  ), {foo:1, bar: 2})
   t.end()
 })
 
@@ -26,15 +42,6 @@ tape('badmap', function (t) {
   t.deepEqual(map(['a', 'b', 'c'], {a: true}), undefined)
   t.end()
 })
-
-
-
-
-
-
-
-
-
 
 
 
