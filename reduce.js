cde469b48a398e03fc29e78a8a503562140992cf
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-02 22:16:21 +1300

    reimplement reduce to use arrays for groupings - this is better because it maps to a stream, so you can have multiple phases in a query, like use a $filter after a $reduce, which is equalivant to a HAVING clause in SQL.

--- reduce.js
+++ reduce.js
@@ -1,6 +1,7 @@
 var u = require('./util')
 var map = u.map
 var simple = require('./basic')
+var search = require('binary-search')
 
 function isFunction (f) { return 'function' === typeof f }
 
@@ -29,19 +30,28 @@ function each(list, iter) {
     iter(list[i], (list.length - i - 1))
 }
 
-//instead of taking the query,
-//this should take a path, and a reduce function.
 function group (g, reduce) {
+
+  function compare (a, b) {
+    for(var i in g) {
+      var x = u.get(a, g[i])
+      var y = u.get(b, g[i])
+      if(x != y) return x < y ? -1 : 1
+    }
+    return 0
+  }
+
   return function (a, b) {
-    var A = a = a || {}
-    each(g, function (k, notLast) {
-      var v = u.get(b, k)
-      A[v] = !notLast ? reduce(A[v], b) : A[v] || {}
-      A = A[v]
-    })
+    var A = a = a || []
+    var i = search(A, b, compare)
+
+    if(i >= 0) A[i] = reduce(A[i], b)
+    else       A.splice(~i, 0, reduce(undefined, b))
+
     return a
   }
 }
+
 
 function make (query) {
   var k = isSimple(query)
@@ -49,15 +59,30 @@ function make (query) {
   else if(u.isObject(query))
     return multi(map(query, function (q, k) {
       if(k == '$group') return undefined
-      return gmake(query[k])
+      return make(query[k])
     }))
+  else return function (a, b) {
+    return b[query]
+  }
 }
 
 function gmake (query) {
- return query.$group ? group(query.$group, make(query)) : make(query)
+  if(isSimple(query)) return make(query)
+
+  var paths = []
+  function isMap (e) {
+    return !isSimple(e) ? (paths.push(e), e) : undefined
+  }
+  function isObj (e) {
+    return u.isObject(e) && !isSimple(e)
+  }
+  var maps = u.project(query, isMap, isObj)
+
+  return paths.length ? group(paths, make(query)) : make(query)
 }
 
 module.exports = gmake
+
 
 
 
