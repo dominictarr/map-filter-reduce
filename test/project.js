0b5e97bfa50cb70c5fa270de2b3cbaff458546ed
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-09 08:56:30 +1200

    more coverage for util.paths

--- test/project.js
+++ test/project.js
@@ -1,6 +1,7 @@
 var tape = require('tape')
-var project = require('../util').project
-var paths = require('../util').paths
+var u = require('../util')
+var project = u.project
+var paths = u.paths
 
 var input = {
   foo: {bar: true, baz: false},
@@ -37,7 +38,31 @@ tape('simple', function (t) {
   t.end()
 })
 
+function truth (e) { return e === true }
+function untruth (e) { return e === false }
+
+function not(fn) {
+  return function (v) { return !fn(v) }
+}
+
+var input2 = {
+  foo: {bar: true, baz: false},
+  baz: false,
+  foofoo: {whatever: false, okay: true},
+  numbers: [1,2,3]
+}
+
 tape('paths', function (t) {
-  t.deepEqual(paths(input, function (e) { return e===true }), [['foo', 'bar'], ['foofoo', 'okay']])
+  t.deepEqual(paths(input2, truth), [['foo', 'bar'], ['foofoo', 'okay']])
+  t.deepEqual(paths(input2, untruth), [['foo', 'baz'],'baz', ['foofoo', 'whatever']])
+  t.deepEqual(paths(input2, u.isObject), [])
+  t.deepEqual(paths(input2, u.isBasic), [
+    [ 'foo', 'bar' ], [ 'foo', 'baz' ], 'baz',
+    [ 'foofoo', 'whatever' ], [ 'foofoo', 'okay' ] ]
+  )
+  t.deepEqual(paths({okay: true}, truth), ['okay'])
+  t.deepEqual(paths({okay: true}, untruth), [])
   t.end()
 })
+
+
