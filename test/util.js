9d53f31d2de028f2d5a014855e8d1761cb24e5c3
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-07 16:12:17 +1300

    test:ranges

--- test/util.js
+++ test/util.js
@@ -22,9 +22,16 @@ tape('u', function (t) {
 tape('ranges', function (t) {
   t.ok(u.isRange({$lt: 'a'}))
   t.equal(u.upper({$lt: 'a'}), 'a')
+  t.equal(u.lower({$lt: 'a'}), null)
+  t.equal(u.upper({$lte: 'a'}), 'a')
+  t.equal(u.lower({$lte: 'a'}), null)
+
+  t.equal(u.upper({$gt: 'a'}), undefined)
+  t.equal(u.lower({$gt: 'a'}), 'a',)
+  t.equal(u.upper({$gte: 'a'}), undefined)
+  t.equal(u.lower({$gte: 'a'}), 'a')
   t.end()
 })
-
 
 
 
